5c17f80a4572a11f2fe1bb5559bb912079c2f4a2
Sumit_b_o <sumitbrnwl48@gmail.com>
2018-10-04 02:15:02 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -124,6 +124,7 @@ public class MainActivity extends AppCompatActivity {
     private void calculatePrice()
     {
        price=quantity* priceOfOne();
+        return ;
     }
 
     private void Ck()
